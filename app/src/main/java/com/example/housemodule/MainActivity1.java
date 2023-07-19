cd60eb74046638a0e07083767e78d30d5068b729
zmzwiwj <zmzwiwj@zmzwiwjdebijibendiannao.local>
2023-07-19 10:13:37 +0800

    first commit

---
+++ app/src/main/java/com/example/housemodule/MainActivity1.java
@@ -0,0 +1,24 @@
+package com.example.housemodule;
+
+import android.os.Bundle;
+
+import androidx.activity.ComponentActivity;
+import androidx.annotation.Nullable;
+
+/**
+ * <pre>
+ *     author : zmzwiwj
+ *     e-mail : xxx@xx
+ *     time   : 2023/07/19
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class MainActivity1 extends ComponentActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_layout);
+    }
+}
